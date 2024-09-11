1d6ef9dd2be444b9581c805dd0de4d59ac34972e
Takuya <takuya2190ayukat@gmail.com>
2024-09-11 10:31:45 +0900

    Initial Commit

---
+++ RecipeSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeSwiftUI
+//
+//  Created by Takuya Fuma on 2024/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeSwiftUI/RecipeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSwiftUIApp.swift
+//  RecipeSwiftUI
+//
+//  Created by Takuya Fuma on 2024/09/11.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
